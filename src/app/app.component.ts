2ce2d23e25e3f68080b39f0f37006fa755374ff8
wassim-saadallah <wassim.saadalla@gmail.com>
2018-09-24 20:04:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
+import { Time } from '@angular/common';
 
+
+export class Chat{
+  constructor(private from: 'user' | 'bot', private body: string, private time: Date){
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  value = 'aerty';
+  messages : Chat[];
+  current = 'bot';
+  value : string;
+
+  constructor(){
+    this.messages = [new Chat('bot', 'test', new Date())];
+    this.value = "";
+  }
 
   send(){
-    console.log(this.value)
+    let from = this.current;
+    this.current = this.current === 'bot' ? 'user' : 'bot';
+    this.messages.push(new Chat('bot', this.value, new Date()));
+    console.log(this.messages);
   }
 }
