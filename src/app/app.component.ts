4335a55874a6bc437abcb563e6d1c78c7204eba1
wassim-saadallah <wassim.saadalla@gmail.com>
2018-09-24 17:19:38 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hotel-booking-chatbot';
+  value = 'aerty';
+
+  send(){
+    console.log(this.value)
+  }
 }
